8185b0552f1dee2f1f488f15cb9ed1dffd20f474
tbs99 <timsregisters@gmail.com>
2020-10-05 16:41:43 +0300

    new structure

--- form.js
+++ form.js
@@ -16,14 +16,16 @@ var open_form = function(){
 }
 
 var set_validation_message = function(object, text, value){
+  var error_exp = new RegExp("_empty");
+  var old_error = value.replace(error_exp, "");
+  $('.validation_message[id="'+ old_error +'"]').remove();
   var left = parseInt(object.position().left);
   var top = parseInt(object.position().top);
   if(!$('.validation_message[id="'+value+'"]').length){
     var message = $('<div class="validation_message d-flex justify-content-center align-items-center" id="' + value + '"><p id class="m-2">'+ text +'</p></div>').css({"left":left, "top":top+40});
-    var warning_sign = $('<img class="warning_sign mt-1" id='+ value +' src=images/warning.png width="30px" height="26px">').css({"left":left+460, "top":top});
+    var warning_sign = $('<img class="warning_sign mt-1" id='+ value +' src=images/warning.png width="30px" height="26px">').css({"left":left-30, "top":top});
     $(".form").append(message);
     $(".form").append(warning_sign);
-    console.log(text);
   }
   else{
     $('.validation_message[id="'+value+'"]>p').text(text);
@@ -32,6 +34,31 @@ var set_validation_message = function(object, text, value){
   $('.validation_message[id="'+value+'"]').effect("shake");
 }
 
+var empty_check = function(selector){
+  if($("#"+selector).val().length == 0){
+    set_validation_message($("#"+selector),"field can't be empty", selector+"_er_empty")
+  }
+  else{
+    $("#"+selector+"_er_empty").remove();
+    $("#"+selector+"_er_empty").remove();
+  }
+}
+
+var clear_warnings = function(selector){
+  $(selector).remove();
+  $(selector).remove();
+  $(selector + "_empty").remove();
+  $(selector + "_empty").remove();
+
+}
+var check_fill_forms = function(){
+  empty_check("name");
+  empty_check("phone");
+  empty_check("country");
+  empty_check("city");
+  empty_check("country");
+  empty_check("postal_code");
+}
 $(".exit_form").on("click",function(){
   close_form();
 })
@@ -41,8 +68,7 @@ $("#name").on("blur",function(){
     set_validation_message($(this), "Name can't be that short", "name_er");
   }
   else{
-    $("#name_er").remove();
-    $("#name_er").remove();
+    clear_warnings("#name_er");
   }
 });
 
@@ -54,28 +80,29 @@ $("#phone").on("blur",function(){
     set_validation_message($(this),'Phone has to start with "+"', "phone_er");
   }
   else{
-    $("#phone_er").remove();
-    $("#phone_er").remove();
+    clear_warnings("#phone_er");
   }
 });
+
 $("#country").on("blur",function(){
   if($(this).val().length < 3){
     set_validation_message($(this), "Country name can't be that short", "country_er");
   }
   else{
-    $("#country_er").remove();
-    $("#country_er").remove();
+    clear_warnings("#country_er");
   }
 });
+
 $("#city").on("blur",function(){
   if($(this).val().length < 3){
     set_validation_message($(this), "City name can't be that short", "city_er");
   }
   else{
-    $("#city_er").remove();
-    $("#city_er").remove();
+    clear_warnings("#city_er");
   }
-});
+  }
+);
+
 $("#postal_code").on("blur",function(){
   if($(this).val().length < 5){
     set_validation_message($(this),"Post Code is wrong", "post_code_er");
@@ -87,7 +114,24 @@ $("#postal_code").on("blur",function(){
     set_validation_message($(this),"Post Code is wrong", "post_code_er");
   }
   else{
-    $("#post_code_er").remove();
-    $("#post_code_er").remove();
+    clear_warnings("#postal_code_er");
   }
 });
+
+$(".order_shipping").on("click", function(){
+  check_fill_forms();
+  if($(".validation_message").length){
+    $(".validation_message").effect("shake");
+    $(".warning_sign").effect("shake");
+  }
+  else{
+  close_form();
+  $(".alert_cart").text($(this).prev().prev().text() + "Your order has'been sucessfully sent to the manager");
+  $(".alert_cart").css({"visibility":"visible"});
+  $(".alert_cart").animate({opacity: "-0"},5000,function(){
+    $(this).css({"opacity":"1","visibility":"hidden"});
+  });
+  myStorage.clear();
+}
+}
+);
